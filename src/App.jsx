a805cda489e5b469eab1b8ca22be278c6420f216
Nathanael <nathanael.vinicius03@gmail.com>
2026-09-12 10:55:24 -0300

    feat: cria e estiliza seção sobre

--- src/App.jsx
+++ src/App.jsx
@@ -71,6 +71,18 @@ function App() {
         </div>
       </section>
 
+      <section id="sobre">
+        <div className="section-heading">
+          <p>Sobre nós</p>
+
+          <h2>Mais do que tatuagem.</h2>
+        </div>
+
+        <p>
+          Um espaço edicado à arte, identidade e expressão através da tatuagem.
+        </p>
+      </section>
+
     </main>
   )
 }

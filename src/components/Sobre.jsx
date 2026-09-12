cae92379c35a698d96f605562edcd2a745ab0e47
Nathanael <nathanael.vinicius03@gmail.com>
2026-09-12 12:47:15 -0300

    refactor: separa sobre em componente

---
+++ src/components/Sobre.jsx
@@ -0,0 +1,17 @@
+function Sobre() {
+    return (
+        <section id="sobre">
+        <div className="section-heading">
+          <p>Sobre nós</p>
+
+          <h2>Mais do que tatuagem.</h2>
+        </div>
+
+        <p>
+          Um espaço dedicado à arte, identidade e expressão através da tatuagem.
+        </p>
+      </section>
+    )
+}
+
+export default Sobre

26fd19b27c0f3d2451e975be6cd79b301c170a8e
Nathanael <nathanael.vinicius03@gmail.com>
2026-09-12 12:22:50 -0300

    refactor: integra hero na home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,12 @@
 import Header from '../components/Header';
+import Hero from '../components/Hero';
 
 function Home() {
     return (
     <main>
       <Header />
 
-      <section className="hero" id="inicio">
-        <div className="hero-content">
-          <p className="hero-label">ESTÚDIO DE TATUAGEM</p>
-
-        <h1>
-          Tatuagem não é só
-          <br />
-          uma marca.
-        </h1>
-
-        <h2>É uma história na pele.</h2>
-        <p>
-          Arte, identidade e experiências personalizadas
-          para transformar ideias em tatuagens.
-        </p>
-
-        <button type="button">
-          Agendar horário
-        </button>
-        </div>
-      </section>
+      <Hero />
 
       <section id="trabalhos">
         <div className="section-heading">

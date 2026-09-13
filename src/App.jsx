68773d10b431b53a35beadd072151e468098f3b6
Nathanael <nathanael.vinicius03@gmail.com>
2026-09-13 18:15:12 -0300

    style(cadastro): cria layout inicial da tela de cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Cadastro from './pages/Cadastro';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Login" element={<Login />} />
+        <Route path="/Cadastro" element={<Cadastro />} />
       </Routes>
     </BrowserRouter>
   )

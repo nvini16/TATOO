fd0b5717a4128ad31d07351f751ee97ce3536683
Nathanael <nathanael.vinicius03@gmail.com>
2026-09-17 14:40:07 -0300

    feat: cria galeria para trabalhos ajustes na legenda, substituição de imagem e titulo

--- src/components/Trabalhos.jsx
+++ src/components/Trabalhos.jsx
@@ -4,6 +4,12 @@ import trabalho3 from '../assets/trabalhos/trabalho-3.jpg';
 import trabalho4 from '../assets/trabalhos/trabalho-4.jpg';
 import trabalho5 from '../assets/trabalhos/trabalho-5.jpg';
 import trabalho6 from '../assets/trabalhos/trabalho-6.webp';
+import trabalho7 from '../assets/trabalhos/trabalho-7.jpg';
+import trabalho8 from '../assets/trabalhos/trabalho-8.jpg';
+import trabalho9 from '../assets/trabalhos/trabalho-9.jpg'
+import trabalho10 from '../assets/trabalhos/trabalho-10.jpg'
+import trabalho11 from '../assets/trabalhos/trabalho-11.jpg'
+import trabalho12 from '../assets/trabalhos/trabalho-12.jpg'
 
 function Trabalhos() {
   return (
@@ -16,7 +22,7 @@ function Trabalhos() {
           </p>
 
           <h2>
-            Portifólio Autoral
+            Portifólio
           </h2>
 
           <div className="trabalhos-divider"></div>
@@ -55,7 +61,7 @@ function Trabalhos() {
           <div className="trabalhos-info">
             <span>Blackwork</span>
 
-            <h3>Tatuagem autoral</h3>
+            <h3>Flor no Vaso</h3>
           </div>
         </article>
 
@@ -69,7 +75,7 @@ function Trabalhos() {
 
           <div className="trabalhos-info">
             <span>Preto e Cinza</span>
-            <h3>Tatuagem autoral</h3>
+            <h3>Braço Florido</h3>
           </div>
         </article>
 
@@ -83,7 +89,7 @@ function Trabalhos() {
 
           <div className="trabalhos-info">
             <span>Cybertribal</span>
-            <h3>Tatuagem autoral</h3>
+            <h3>Cyber</h3>
           </div>
         </article>
 
@@ -97,7 +103,7 @@ function Trabalhos() {
 
           <div className="trabalhos-info">
             <span>Blackwork</span>
-            <h3>Tatuagem autoral</h3>
+            <h3>Gato Anjo Noturno</h3>
           </div>
         </article>
 
@@ -111,7 +117,7 @@ function Trabalhos() {
 
           <div className="trabalhos-info">
             <span>Blackwork</span>
-            <h3>Tatuagem autoral</h3>
+            <h3>Tigre Black</h3>
           </div>
         </article>
 
@@ -125,7 +131,91 @@ function Trabalhos() {
 
           <div className="trabalhos-info">
             <span>Fine Line</span>
-            <h3>Tatuagem autoral</h3>
+            <h3>Mãe Pai Infinito</h3>
+          </div>
+        </article>
+
+        <article className="trabalho-card">
+          <div className="trabalhos-imagem">
+            <img 
+              src={trabalho7}
+              alt="Trabalho de tatuagem"
+            />
+          </div>
+
+          <div className="trabalhos-info">
+            <span>Blackwork | Autoral</span>
+            <h3>Gato morcego</h3>
+          </div>
+        </article>
+
+        <article className="trabalho-card">
+          <div className="trabalhos-imagem">
+            <img 
+              src={trabalho8}
+              alt="Trabalho de tatuagem"
+            />
+          </div>
+
+          <div className="trabalhos-info">
+            <span>Blackwork</span>
+            <h3>Laroyê</h3>
+          </div>
+        </article>
+
+        <article className="trabalho-card">
+          <div className="trabalhos-imagem">
+            <img 
+              src={trabalho9}
+              alt="Trabalho de tatuagem"
+            />
+          </div>
+
+          <div className="trabalhos-info">
+            <span>Blackwork</span>
+            <h3>Laroyê</h3>
+          </div>
+        </article>
+
+        <article className="trabalho-card">
+          <div className="trabalhos-imagem">
+            <img 
+              src={trabalho10}
+              alt="Trabalho de tatuagem"
+            />
+          </div>
+
+          <div className="trabalhos-info">
+            <span>Blackwork</span>
+            <h3>Laroyê</h3>
+          </div>
+        </article>
+
+        <article className="trabalho-card">
+          <div className="trabalhos-imagem">
+            <img 
+              src={trabalho11}
+              alt="Trabalho de tatuagem"
+            />
+          </div>
+
+          <div className="trabalhos-info">
+            <span>Blackwork</span>
+            <h3>Laroyê</h3>
+          </div>
+        </article>
+
+        <article className="trabalho-card">
+          <div className="trabalhos-imagem">
+            <img 
+              src={trabalho12}
+              alt="Trabalho de tatuagem"
+            />
+          </div>
+
+          <div className="trabalhos-info">
+            <span>Blackwork</span>
+            <h3>Laroyê</h3>
           </div>
         </article>
       </div>

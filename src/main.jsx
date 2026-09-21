d92c1176ad7b79453c4aa7b6bb9b6b79d8c3df29
Nathanael <nathanael.vinicius03@gmail.com>
2026-09-21 09:46:39 -0300

    refactor: desabilitar index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/global.css'
+import './styles/header.css'
+import './styles/hero.css'
+import './styles/agendamento.css'
+import './styles/animations.css'
+import './styles/cadastro.css'
+import './styles/login.css'
+import './styles/responsive.css'
+import './styles/sobre.css'
+import './styles/trabalhos.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

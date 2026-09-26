5d4aff4d2785fa97a9d67b5277b3c7da555c2514
Nathanael <nathanael.vinicius03@gmail.com>
2026-09-26 11:11:25 -0300

    style: estrutura e estilização inicial da seção de cuidados

--- src/components/Cuidados.jsx
+++ src/components/Cuidados.jsx
@@ -1,6 +1,33 @@
 function Cuidados() {
     return (
-        <section></section>
+        <section className="cuidados">
+            <h2>Cuidados</h2>
+
+            <p className="cuidados-introducao">
+                Saiba como preparar sua pele antes da tatuagem e cuidar dela durante a cicatrização.
+            </p>
+
+            <div className="cuidados-conteudo">
+                <article className="">
+                    <h3>Antes da tatuagem</h3>
+                    
+                    <ul>
+                        <li>Durma bem na noite anterior.</li>
+                        <li>Alimente-se antes do procedimento.</li>
+                        <li>Beba bastante água.</li>
+                        <li>Evite consumir álcool antes da sessão.</li>
+                        <li>Não aplique cremes ou produtos irritantes na região.</li>
+                    </ul>
+                </article>
+
+                <article className="cuidados-card">
+                    <h3>Depois da tatuagem</h3>
+                    <p>
+                        Siga os cuidados necessários para uma boa cicatrização.
+                    </p>
+                </article>
+            </div>
+        </section>
     )
 }
 
